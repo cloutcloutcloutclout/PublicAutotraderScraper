087e2257e1fe516561960a290d24fd06253ba311
Zain <zainhu06@hotmail.com>
2026-09-13 17:28:23 +0100

    Main - changes

--- main.py
+++ main.py
@@ -1,23 +1,64 @@
-import requests
-from bs4 import BeautifulSoup
+# env
 import os
 from dotenv import load_dotenv
+# scrape
+import requests
+from bs4 import BeautifulSoup
+from playwright.sync_api import sync_playwright
 # init
 
 # loadenv
 load_dotenv()
-url = os.getenv('HardcodeURL')
+url = os.getenv("origin")
 
-# soup
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
+# Find the listings
+with sync_playwright() as p:
+  browser = p.chromium.launch(
+      headless=False, args=["--disable-blink-features=AutomationControlled"]
+  )
 
-carName = soup.find("h1", class_="sc-1n64n0d-8 sc-j1c9qm-2 gmXvZp kzofPy")
-carDesc = soup.find("span", class_="sc-1n64n0d-7 sc-j1c9qm-4 kDYJWK fiumgF")
+  context = browser.new_context(
+      viewport={"width": 1920, "height": 1080},
+      user_agent=(
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML,"
+          " like Gecko) Chrome/122.0.0.0 Safari/537.36"
+      ),
+  )
 
-# if its weird like this do this -> 
-price = soup.find("p", attrs={"data-testid": "advert-price"}, class_="sc-1n64n0d-5 sc-1t1ktfs-0 hQsESP etMJIu")
+  # Anti-detection flag override
+  context.add_init_script("""
+        Object.defineProperty(navigator, 'webdriver', {
+            get: () => undefined
+        });
+    """)
 
-print(carName.get_text())
-print(carDesc.get_text())
-print(price.get_text())
+  page = context.new_page()
+  page.goto(url, wait_until="networkidle")
+
+  # Extract all href attributes
+  links = page.locator("a").evaluate_all(
+      "elements => elements.map(el => el.href)"
+  )
+
+  browser.close()
+
+# Filter to ONLY include links containing '.co.uk/car-details/'
+car_links = sorted(
+    list(
+        set(link for link in links if link and ".co.uk/car-details/" in link)
+    )
+)
+
+# Loop and find specific vehicle information
+
+for i in range(len(car_links)):
+  response = requests.get(car_links[i])
+  soup = BeautifulSoup(response.content, 'html.parser')
+
+
+
+  carName = soup.find("h1", class_="sc-1n64n0d-8 sc-j1c9qm-2 gmXvZp kzofPy")
+  carDesc = soup.find("span", class_="sc-1n64n0d-7 sc-j1c9qm-4 kDYJWK fiumgF")
+  price = soup.find("p", attrs={"data-testid": "advert-price"}, class_="sc-1n64n0d-5 sc-1t1ktfs-0 hQsESP etMJIu")
+
+  print(carName.get_text(), carDesc.get_text(), price.get_text())
